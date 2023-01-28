209039493362a75810c2b60a050a3dad2a37a87e
Wes Stephenson <29235667+wesstep@users.noreply.github.com>
2023-01-28 13:09:04 -0700

    made contact info harder for spammers to exploit

--- assets/scripts/components/Contact.js
+++ assets/scripts/components/Contact.js
@@ -1,4 +1,11 @@
 export default class Contact extends HTMLElement {
+    #num1 = '534';
+    #num2 = '355';
+    #num3 = '8375';
+    #mail1 = '5131petssew';
+    #mail2 = '@gmail.com';
+    #emailEl = new DOMParser()
+        .parseFromString(`<a href="mailto:${this.getEmail()}">${this.getEmail()}</a>`, 'text/html');
     #template = `
     <style>
         /*
@@ -37,6 +44,31 @@ export default class Contact extends HTMLElement {
             color: blue;
         }
         
+        .inline-block {
+            display: inline-block;
+        }
+        
+        .btn {
+            border: 0;
+            background-color: navy;
+            border-radius: 0.25rem;
+            color: white;
+            display: inline-block;
+            margin: 0.5rem 0 0 0;
+            padding: 0.25rem 0.5rem;
+            transition: 0.2s;
+            cursor: pointer;
+        }
+        
+        .btn:hover {
+            background-color: #555;
+            transition: 0.2s;
+        }
+        
+        .display-none {
+            display: none;
+        }
+        
         h1, h2, h3, h4, h5, h6 {
             font-family: 'Playfair Display', serif;
         }
@@ -55,15 +87,51 @@ export default class Contact extends HTMLElement {
     <section>
         <h2>Contact</h2>
         <p>Feel free to reach out! Please no spam.</p>
-        <p>Call or Text: <a href="tel:14355535738">435-553-5738</a></p>
-        <p>Email: <a href="mailto:wesstep1315@gmail.com">wesstep1315@gmail.com</a></p>
+        <div>
+            <p class="inline-block">Call or Text: </p>
+            <button class="btn" type="button" id="get-number-btn">Get Number</button>
+            <a href="tel:${this.getNumber()}" id="num" class="inline-block display-none">${this.getNumber()}</a>
+        </div>
+        <div>
+            <p class="inline-block">Email: </p>
+            <button class="btn" type="button" id="get-email-btn">Get Email</button>
+            <a href="mailto:${this.getEmail()}" id="email" class="inline-block display-none">${this.getEmail()}</a>
+        </div>
         <p><a href="https://www.linkedin.com/in/wes-stephenson/">LinkedIn Profile</a></p>
         <p><a href="https://github.com/WesStep">GitHub Profile</a></p>
     </section>
     `;
+
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.innerHTML = this.#template;
+        this.getNumberBtnEl = this.shadowRoot.getElementById('get-number-btn');
+        this.getEmailBtnEl = this.shadowRoot.getElementById('get-email-btn');
+        this.numEl = this.shadowRoot.getElementById('num');
+        this.emailEl = this.shadowRoot.getElementById('email');
+
+        this.getNumberBtnEl.addEventListener('click', this.toggleShowNumber.bind(this));
+        this.getEmailBtnEl.addEventListener('click', this.toggleShowEmail.bind(this));
+    }
+
+    toggleShowNumber() {
+        this.numEl.style.display = (this.numEl.style.display !== 'inline-block') ? 'inline-block' : 'none';
+    }
+
+    toggleShowEmail() {
+        this.emailEl.style.display = (this.emailEl.style.display !== 'inline-block') ? 'inline-block' : 'none';
+    }
+
+    getEmail() {
+        return this.reverseString(this.#mail1) + this.#mail2;
+    }
+
+    getNumber() {
+        return this.reverseString(this.#num1) + this.reverseString(this.#num2) + this.reverseString(this.#num3);
+    }
+
+    reverseString(string) {
+        return string.split('').reverse().join('');
     }
 }
